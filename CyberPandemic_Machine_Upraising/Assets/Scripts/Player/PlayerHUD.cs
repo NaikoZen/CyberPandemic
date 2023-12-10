6f81b742bcad27e1a50c859b16cc4f7523d42df9
NaikoZen <kalleberebeca@gmail.com>
2023-12-10 15:58:36 -0300

    .

---
+++ CyberPandemic_Machine_Upraising/Assets/Scripts/Player/PlayerHUD.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+
+public class PlayerHUD : NetworkBehaviour
+{
+    [SerializeField] private Slider healthSlider;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
